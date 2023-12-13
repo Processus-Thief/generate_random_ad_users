4c5ce8d71c1f550e1b96a304a7d8c5ec5a714b51
Processus <processus.thief@gmail.com>
2023-12-13 11:20:55 +0100

    Add files via upload

--- generate_random_ad_users.ps1
+++ generate_random_ad_users.ps1
@@ -44,6 +44,61 @@ $Users = @(
 )
 
 
+
+function New-RandomPassword {
+[CmdletBinding()]
+#Switch type of parameter allows us to use just -ParameterName for boolean type of parameters
+Param(
+    [Parameter()]
+    [int]$PasswordLength = 15,
+    [Parameter()]
+    [switch]$BigLetters,
+    [Parameter()]
+    [switch]$SmallLetters,
+    [Parameter()]
+    [switch]$Numbers,
+    [Parameter()]
+    [switch]$NormalSpecials,
+    [Parameter()]
+    [switch]$ExtendedSpecials
+)
+    $asciiTable = $null
+    #We are checking if any parameter was provided for function. If not, let's use small leters
+    if (!$BigLetters -and !$SmallLetters -and !$Numbers -and !$NormalSpecials -and !$ExtendedSpecials) {
+        for ( $i = 97; $i -le 122; $i++ ) {
+            $asciiTable += , [char][byte]$i
+        }
+    }
+    if ($Numbers) {
+        for ( $i = 48; $i -le 57; $i++ ) {
+            $asciiTable += , [char]$i
+        }
+    }
+    #Normal specials have better chance to work in passwords used in some exotic environments
+    if ($NormalSpecials) {
+        $asciiTable += "*","$","-","+","?","_","&","=","!","%","{","}","/"
+    }
+    if ($ExtendedSpecials) {
+        $asciiTable += "@","#",".",",","^","(",")",":",";","'","`"","~","``","<",">"
+    }
+    if ($BigLetters) {
+        for ( $i = 65; $i -le 90; $i++ ) {
+            $asciiTable += , [char]$i
+        }
+    }
+    if ($SmallLetters) {
+        for ( $i = 97; $i -le 122; $i++ ) {
+            $asciiTable += , [char]$i
+        }
+    }
+    $tempPassword = $null
+    for ( $i = 1; $i -le $PasswordLength; $i++) {
+        $tempPassword += ( Get-Random -InputObject $asciiTable )
+    }
+    return $tempPassword
+}
+
+
 foreach ($User in $Users) {
 
        $Firstname = $User.split(' ')[0]
@@ -51,7 +106,7 @@ foreach ($User in $Users) {
        $SAM = $User[0]+$Lastname
        $Displayname = $SAM
        $UPN = $Displayname + "@lab.com"
-       $generatedpass = -join ((97..122) | Get-Random -Count 20 | ForEach-Object {[char]$_})
+       $generatedpass = New-RandomPassword -BigLetters -SmallLetters -Numbers -NormalSpecials -PasswordLength 20
        $Password = (ConvertTo-SecureString $generatedpass -AsPlainText -Force)
 
        New-ADUser -Name "$Displayname" -DisplayName "$Displayname" -SamAccountName "$SAM" -UserPrincipalName "$UPN" -GivenName "$Firstname" -Surname "$Lastname" -AccountPassword $Password -Enabled $true -ChangePasswordAtLogon $false -PasswordNeverExpires $true
